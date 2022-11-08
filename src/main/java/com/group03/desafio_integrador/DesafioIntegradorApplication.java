370e3f2ccc56830348863cd2733072805f9c1f1b
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-11-08 13:25:34 -0300

    :hammer: build: initial setup

---
+++ src/main/java/com/group03/desafio_integrador/DesafioIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.group03.desafio_integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioIntegradorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioIntegradorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group03/desafio_integrador/DesafioIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group03.desafio_integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioIntegradorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
