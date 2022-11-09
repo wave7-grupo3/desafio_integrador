3487e4679f37450f9657b5c0c4dd6fd29e7a2952
Gabriel Morais <gabriel.mmorais@mercadolivre.com>
2022-11-09 17:06:18 -0300

    :recycle: refactor: refactor HandlerException

--- src/main/java/com/group03/desafio_integrador/advice/ValidationErrorDetail.java
+++ src/main/java/com/group03/desafio_integrador/advice/ValidationErrorDetail.java
@@ -1,9 +1,11 @@
 package com.group03.desafio_integrador.advice;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ValidationErrorDetail {
     private String field;
     private String message;
--- src/main/java/com/group03/desafio_integrador/advice/exeptions/NotFoundException.java
+++ src/main/java/com/group03/desafio_integrador/advice/exeptions/NotFoundException.java
@@ -1,7 +1,24 @@
 package com.group03.desafio_integrador.advice.exeptions;
 
-public class NotFoundException extends  Exception {
+import com.group03.desafio_integrador.advice.ExceptionDetails;
+import com.group03.desafio_integrador.advice.ValidationErrorDetail;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class NotFoundException extends RuntimeException {
+
+    private List<ValidationErrorDetail> errors;
+
     public NotFoundException(String message) {
         super(message);
     }
+
+    public NotFoundException(String message, List<ValidationErrorDetail> errors) {
+        super(message);
+        this.errors = errors;
+    }
+
+
 }
