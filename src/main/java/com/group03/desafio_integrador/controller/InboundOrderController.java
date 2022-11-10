9a23f6be8e217d98ab57fbdb6c3f71225c3a0b92
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-11-10 08:44:57 -0300

    :memo: docs: created JavaDocs on the controller layer class

--- src/main/java/com/group03/desafio_integrador/controller/InboundOrderController.java
+++ src/main/java/com/group03/desafio_integrador/controller/InboundOrderController.java
@@ -20,16 +20,33 @@ public class InboundOrderController {
     @Autowired
     public IInboundOrderService service;
 
+    /**
+     * Rota responsável pela criação de um pedido de Ordem para o armazem.
+     * @author Gabriel Morais
+     * @param inboundOrder - InboundOrder
+     * @return BatchStockDTO - Retorna um dto do tipo BatchStockDTO.
+     */
     @PostMapping
     public ResponseEntity<BatchStockDTO> save(@Valid @RequestBody InboundOrder inboundOrder) throws Exception {
         return new ResponseEntity<>(service.save(inboundOrder), HttpStatus.CREATED);
     }
 
+    /**
+     * Rota responsável por listar todas os pedidos de ordem para o armazem.
+     * @author Gabriel Morais
+     * @return List<InboundOrder> - Retorna uma lista de Propriedades.
+     */
     @GetMapping
     public ResponseEntity<List<InboundOrder>> getAll() {
         return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
     }
 
+    /**
+     * Rota responsável pela alteração de um lote.
+     * @author Gabriel Morais
+     * @param batch - Batch
+     * @return Batch - Retorna uma entidade do tipo Batch.
+     */
     @PutMapping
     public ResponseEntity<Batch> update(@Valid @RequestBody Batch batch) {
         return new ResponseEntity<>(service.update(batch), HttpStatus.CREATED);
