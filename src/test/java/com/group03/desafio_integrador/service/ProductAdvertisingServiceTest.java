11cd4603481fbd8f0fdc40b8bb622372164d5ddf
Carolina Hakamada <carolina.hakamada@mercadolivre.com>
2022-11-10 14:56:00 -0300

    :white_check_mark: test: successful test cases implementation ProductAdvertisingServiceTest

---
+++ src/test/java/com/group03/desafio_integrador/service/ProductAdvertisingServiceTest.java
@@ -0,0 +1,61 @@
+package com.group03.desafio_integrador.service;
+
+import com.group03.desafio_integrador.entities.Batch;
+import com.group03.desafio_integrador.entities.CategoryEnum;
+import com.group03.desafio_integrador.entities.ProductAdvertising;
+import com.group03.desafio_integrador.entities.Seller;
+import com.group03.desafio_integrador.repository.BatchRepository;
+import com.group03.desafio_integrador.repository.ProductAdvertisingRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductAdvertisingServiceTest {
+
+    @InjectMocks
+    private ProductAdvertisingService productAdvertisingService;
+
+    @Mock
+    private ProductAdvertisingRepository productAdvertisingRepository;
+
+    private ProductAdvertising mockProductAdvertising;
+
+    @BeforeEach
+    void setUp() {
+        mockProductAdvertising = new ProductAdvertising(1L,
+                LocalDate.parse("2022-11-30"),
+                LocalDateTime.of(2022,11, 9, 11,43,0),
+                "Maçã",
+                "teste",
+                Seller.builder().sellerId(1L).build(),
+                CategoryEnum.FS
+                );
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getById_returnSuccess_whenProductAdvertisingExists() {
+        BDDMockito.when(productAdvertisingRepository.findById(ArgumentMatchers.any(Long.class))).thenReturn(Optional.ofNullable(mockProductAdvertising));
+
+        ProductAdvertising productById = productAdvertisingService.getById(1L);
+
+        assertThat(productById).isNotNull();
+        assertThat(productById).isEqualTo(mockProductAdvertising);
+    }
+}
