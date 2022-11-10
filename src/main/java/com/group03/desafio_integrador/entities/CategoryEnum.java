9539642ceedc9d2730af5d3268b6650c90cd9f94
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-11-10 12:12:06 -0300

    :rocket: feat: create method findAllByCategory in layer Repository and Enum Category

--- src/main/java/com/group03/desafio_integrador/entities/CategoryEnum.java
+++ src/main/java/com/group03/desafio_integrador/entities/CategoryEnum.java
@@ -1,11 +1,32 @@
 package com.group03.desafio_integrador.entities;
 
+import com.group03.desafio_integrador.advice.exeptions.NotFoundException;
+
 public enum CategoryEnum {
     FS, // 0
     RF, // 1
-    FF  // 2
+    FF;  // 2
 
 //    FS = Fresco
 //    RF = Refrigerado
 //    FF = Congelado
+
+    /**
+     * Método responsável por converter uma String em Enum.
+     * @author Mariana Saraiva
+     * @return CategoryEnum - Retorna um enum do tipo CategoryEnum.
+     * @throws NotFoundException
+     */
+    public static CategoryEnum toEnum(String string) {
+        CategoryEnum enumCategory;
+
+        try {
+            enumCategory = CategoryEnum.valueOf(string);
+        } catch (Exception ex){
+            throw new NotFoundException("This category is not valid");
+        }
+
+        return enumCategory;
+    }
+
 }
