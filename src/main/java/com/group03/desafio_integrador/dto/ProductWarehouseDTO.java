6915bef6c18a7ac48292ca90b3eea3af7a530d2d
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-11-14 14:28:38 -0300

    :rocket: feat: implements the stock check route of a product in all warehouses

---
+++ src/main/java/com/group03/desafio_integrador/dto/ProductWarehouseDTO.java
@@ -0,0 +1,15 @@
+package com.group03.desafio_integrador.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class ProductWarehouseDTO {
+    private Long productId;
+    private List<WarehouseStockDTO> warehouses;
+}
