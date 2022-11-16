4afb3e39d6b451acc88bd09758e91895c73327d5
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-11-16 15:46:37 -0300

    :rotating_light: fix: fixing warnings generated by Java Docs in IShoppingCartService interface

--- src/main/java/com/group03/desafio_integrador/service/SellerService_.java
+++ src/main/java/com/group03/desafio_integrador/service/SellerService_.java
@@ -1,11 +1,11 @@
 package com.group03.desafio_integrador.service;
 
-import com.group03.desafio_integrador.service.interfaces.ISellerService;
+import com.group03.desafio_integrador.service.interfaces.ISellerService_;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class SellerService_ implements ISellerService {
+public class SellerService_ implements ISellerService_ {
 
 }
