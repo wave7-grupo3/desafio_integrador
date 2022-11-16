0ad480adda5880f5ff4dd3d244c4297778eb6e9f
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-11-16 14:50:43 -0300

    :rotating_light: fix: fixing warnings generated by Java Docs in InboundOrderController

--- src/main/java/com/group03/desafio_integrador/controller/InboundOrderController.java
+++ src/main/java/com/group03/desafio_integrador/controller/InboundOrderController.java
@@ -20,10 +20,10 @@ public class InboundOrderController {
     public IInboundOrderService service;
 
     /**
-     * Rota responsável pela criação de um pedido de Ordem para o armazem.
+     * Rota responsável pela criação de um pedido de Ordem para o armazém.
      * @author Gabriel Morais
      * @param inboundOrder - InboundOrder
-     * @return BatchStockDTO - Retorna um dto do tipo BatchStockDTO.
+     * @return Retorna um dto do tipo BatchStockDTO.
      */
     // TODO: fazer DTO de inboundOrder
     @PostMapping
@@ -32,9 +32,9 @@ public class InboundOrderController {
     }
 
     /**
-     * Rota responsável por listar todas os pedidos de ordem para o armazem.
+     * Rota responsável por listar todos os pedidos de ordem para o armazém.
      * @author Gabriel Morais
-     * @return List<InboundOrder> - Retorna uma lista de Propriedades.
+     * @return Retorna uma lista de Propriedades.
      */
     @GetMapping
     public ResponseEntity<List<InboundOrder>> getAll() {
@@ -45,7 +45,7 @@ public class InboundOrderController {
      * Rota responsável pela alteração de um lote.
      * @author Gabriel Morais
      * @param batch - Batch
-     * @return Batch - Retorna uma entidade do tipo Batch.
+     * @return Retorna uma entidade do tipo Batch.
      */
     // TODO: fazer DTO batch
     @PutMapping
