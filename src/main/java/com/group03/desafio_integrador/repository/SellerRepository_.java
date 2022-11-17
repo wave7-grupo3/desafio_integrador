a9d366bcb5276b1a03d88c494fad47ba9de67bec
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-11-17 08:08:14 -0300

    :rotating_light: fix: fix warnings generated by Java Docs in IBatchService

--- src/main/java/com/group03/desafio_integrador/repository/SellerRepository_.java
+++ src/main/java/com/group03/desafio_integrador/repository/SellerRepository_.java
@@ -3,5 +3,5 @@ package com.group03.desafio_integrador.repository;
 import com.group03.desafio_integrador.entities.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SellerRepository extends JpaRepository<Seller, Long> {
+public interface SellerRepository_ extends JpaRepository<Seller, Long> {
 }
