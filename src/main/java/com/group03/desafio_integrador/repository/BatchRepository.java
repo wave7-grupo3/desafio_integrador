91c47c8443f68955f4307dae5ab85690f9cb45a1
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-11-17 12:28:05 -0300

    :rocket: feat: implements JavaDocs in BatchRepository

--- src/main/java/com/group03/desafio_integrador/repository/BatchRepository.java
+++ src/main/java/com/group03/desafio_integrador/repository/BatchRepository.java
@@ -1,6 +1,5 @@
 package com.group03.desafio_integrador.repository;
 
-import com.group03.desafio_integrador.dto.BatchDueDateDTO;
 import com.group03.desafio_integrador.entities.Batch;
 import com.group03.desafio_integrador.entities.ProductAdvertising;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +10,21 @@ import java.util.List;
 
 @Repository
 public interface BatchRepository extends JpaRepository<Batch, Long> {
+    /**
+     * Método responsável por listar os lotes de produto conforme o Id do Produto
+     * @author Amanda Zotelli
+     * @param id - ProductAdvertising
+     * @return Retorna uma Lista da entidade do tipo Batch.
+     */
     List<Batch> findAllByProductId(ProductAdvertising id);
+
+    /**
+     * Método responsável por listar os lotes de produto que possuem data de
+     * validade maior que o parâmetro informado.
+     * @author Amanda Zotelli
+     * @param expirationDate - LocalDate
+     * @return Retorna uma Lista da entidade do tipo Batch.
+     */
     List<Batch> findAllByExpirationDateGreaterThan(LocalDate expirationDate);
 
 }
