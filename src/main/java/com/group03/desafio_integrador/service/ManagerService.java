2065d17538d139f3f996cf3c19d0816502e81887
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-11-17 08:18:08 -0300

    :rotating_light: fix: fix warnings generated by Java Docs in IProductAdvertisingService

--- src/main/java/com/group03/desafio_integrador/service/ManagerService.java
+++ src/main/java/com/group03/desafio_integrador/service/ManagerService.java
@@ -1,13 +1,13 @@
 package com.group03.desafio_integrador.service;
 
 import com.group03.desafio_integrador.repository.ManagerRepository;
-import com.group03.desafio_integrador.service.interfaces.IManagerService;
+import com.group03.desafio_integrador.service.interfaces.IManagerService_;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ManagerService implements IManagerService {
+public class ManagerService implements IManagerService_ {
 
     private ManagerRepository repository;
 }
