61325d45ea923266a6d10dc74f46d3cc54736dfe
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-11-17 08:15:46 -0300

    :rotating_light: fix: fix warnings generated by Java Docs in IInboundOrderService

--- src/main/java/com/group03/desafio_integrador/service/interfaces/IInboundOrderService.java
+++ src/main/java/com/group03/desafio_integrador/service/interfaces/IInboundOrderService.java
@@ -11,46 +11,45 @@ import java.util.List;
 public interface IInboundOrderService {
 
     /**
-     * Método responsável por salvar um novo lote no pedido de ordem do armazem.
+     * Método responsável por salvar um novo lote no pedido de ordem do armazém.
      * @author Gabriel Morais
-     * @param InboundOrder - inboundOrder
+     * @param inboundOrder - InboundOrder
      * @return BatchStockDTO - Retorna um dto do tipo BatchStockDTO.
-     * @throws Exception
+     * @throws NotFoundException - NotFoundException
      */
-
     // TODO: implementar Exception específica
     BatchStockDTO save(InboundOrder inboundOrder) throws Exception;
 
     /**
      * Método responsável por listar todos os pedidos de ordem do armazém.
      * @author Gabriel Morais
-     * @return List<InboundOrder> - Retorna uma entidade do tipo InboundOrder.
+     * @return Retorna uma lista de entidades do tipo InboundOrder.
      */
     List<InboundOrder> getAll();
 
     /**
      * Método responsável por atualizar as informações do lote contido no armazém.
      * @author Gabriel Morais
-     * @param Batch - batch
-     * @return Batch - Retorna uma entidade do tipo Batch.
+     * @param batch - Batch
+     * @return Retorna uma entidade do tipo Batch.
      */
     Batch update(Batch batch);
 
     /**
      * Método responsável por retornar uma lista de ProductWarehouseStockDTO com os lotes de determinado produto .
      * @author Carolina Hakamada
-     * @param Long - productId
-     * @return List<ProductWarehouseStockDTO> - Retorna uma lista de ProductWarehouseDTO com todos os lotes de um produto em seu armazém e seção.
-     * @throws Exception
+     * @param productId - Long
+     * @return Retorna uma lista de ProductWarehouseDTO com todos os lotes de um produto em seu armazém e seção.
+     * @throws Exception - Exception
      */
     List<ProductWarehouseStockDTO> getAllProductWarehouseStock(Long productId) throws Exception;
 
     /**
      * Método responsável por retornar uma lista de ProductWarehouseStockDTO com lotes ordenados.
      * @author Amanda Zotelli, Rosalia Padoin
-     * @param List<ProductWarehouseStockDTO> - productWarehouseStockDTOList
-     * @param String - sorting
-     * @return List<ProductWarehouseStockDTO> - Retorna uma lista de ProductWarehouseDTO ordenada pelo parâmetro definido.
+     * @param productWarehouseStockDTOList - lista de entidades do tipo ProductWarehouseStockDTO
+     * @param sorting - String
+     * @return Retorna uma lista de ProductWarehouseDTO ordenada pelo parâmetro definido.
      */
     List<ProductWarehouseStockDTO> getAllOrdinancesForBatches(List<ProductWarehouseStockDTO> productWarehouseStockDTOList, String sorting);
 }
