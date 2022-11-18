5a2bfa96124c6cee5848e3b78494bca93bafb536
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-11-18 16:00:35 -0300

    :rocket: feat: implements methods on ISellerService requirement 6

--- src/main/java/com/group03/desafio_integrador/service/interfaces/ISellerService.java
+++ src/main/java/com/group03/desafio_integrador/service/interfaces/ISellerService.java
@@ -1,4 +1,20 @@
 package com.group03.desafio_integrador.service.interfaces;
 
+import com.group03.desafio_integrador.dto.ProductQuantitySellerDTO;
+import com.group03.desafio_integrador.entities.Seller;
+
+import java.util.List;
+
 public interface ISellerService {
+
+    List<Seller> getAll();
+    Seller getById(Long id);
+
+    Seller save(Seller seller);
+
+    Seller update(Seller seller);
+
+    void deleteById(Long id);
+
+    List<ProductQuantitySellerDTO>  filterproductsMoreQuantityPerSeller(Long id);
 }
