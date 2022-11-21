2eab5f0f8cec536427632c25cacee8d8e134d508
rosalia-oliveira <rosalia.oliveira@mercadolivre.com>
2022-11-21 11:55:44 -0300

    :rocket: feat: implements CRUD and Java Docs ManagerService and IMangerService

--- src/main/java/com/group03/desafio_integrador/service/ManagerService.java
+++ src/main/java/com/group03/desafio_integrador/service/ManagerService.java
@@ -1,11 +1,11 @@
 package com.group03.desafio_integrador.service;
 
+import com.group03.desafio_integrador.advisor.exceptions.NotFoundException;
 import com.group03.desafio_integrador.advisor.exceptions.UnprocessableEntityException;
 import com.group03.desafio_integrador.entities.Manager;
 import com.group03.desafio_integrador.repository.ManagerRepository;
 import com.group03.desafio_integrador.service.interfaces.IManagerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -35,6 +36,12 @@ public class ManagerService implements IManagerService, UserDetailsService {
                 .User(managerByUsername.getUsername(), managerByUsername.getPassword(), new ArrayList<>());
     }
 
+    /**
+     * Método responsável pela inclusão de um novo representante no banco de dados.
+     * @author Rosalia Padoin
+     * @param manager - Manager
+     * @throws UnprocessableEntityException - UnprocessableEntityException
+     */
     @Override
     public void saveManager(Manager manager) {
         Manager managerName = managerRepository.findByName(manager.getName());
@@ -43,5 +50,57 @@ public class ManagerService implements IManagerService, UserDetailsService {
         }
         manager.setPassword(passwordEncoder.encode(manager.getPassword()));
         managerRepository.save(manager);
+    }
+
+    /**
+     * Método responsável pela atualização de um representante no banco de dados.
+     * @author Rosalia Padoin
+     * @param manager - Manager
+     * @throws NotFoundException - NotFoundException
+     */
+    @Override
+    public Manager updateManager(Manager manager) {
+        managerRepository.findById(manager.getManagerId())
+                .orElseThrow(() -> new NotFoundException("Manager not found!"));
+
+        return managerRepository.save(manager);
+    }
+
+    /**
+     * Método responsável por listar todos os representantes cadastrados.
+     * @author Rosalia Padoin
+     * @return Retorna uma lista contendo entidades do tipo Manager.
+     * @throws NotFoundException - NotFoundException
+     */
+    @Override
+    public List<Manager> getAll() {
+        List<Manager> allManagers = managerRepository.findAll();
+        if (allManagers.isEmpty()) {
+            throw new NotFoundException("This search did not found any result!");
+        }
+        return allManagers;
+    }
+
+    /**
+     * Método responsável por buscar um representante conforme Id informado.
+     * @author Rosalia Padoin
+     * @return Retorna uma entidade do tipo Manager.
+     * @throws NotFoundException - NotFoundException
+     */
+    @Override
+    public Manager getManagerById(Long id) {
+        return managerRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Manager by id " + id + " not found!"));
+    }
+
+    /**
+     * Método responsável por excluir um representante conforme Id informado.
+     * @author Rosalia Padoin
+     * @throws NotFoundException - NotFoundException
+     */
+    @Override
+    public void deleteManager(Long id) {
+        getManagerById(id);
+        managerRepository.deleteById(id);
     }
 }
--- src/main/java/com/group03/desafio_integrador/service/interfaces/IManagerService.java
+++ src/main/java/com/group03/desafio_integrador/service/interfaces/IManagerService.java
@@ -1,7 +1,48 @@
 package com.group03.desafio_integrador.service.interfaces;
 
+import com.group03.desafio_integrador.advisor.exceptions.NotFoundException;
+import com.group03.desafio_integrador.advisor.exceptions.UnprocessableEntityException;
 import com.group03.desafio_integrador.entities.Manager;
 
+import java.util.List;
+
 public interface IManagerService {
+    /**
+     * Método responsável pela inclusão de um novo representante no banco de dados.
+     * @author Rosalia Padoin
+     * @param manager - Manager
+     * @throws UnprocessableEntityException - UnprocessableEntityException
+     */
     void saveManager(Manager manager);
+
+    /**
+     * Método responsável pela atualização de um representante no banco de dados.
+     * @author Rosalia Padoin
+     * @param manager - Manager
+     * @throws NotFoundException - NotFoundException
+     */
+    Manager updateManager(Manager manager);
+
+    /**
+     * Método responsável por listar todos os representantes cadastrados.
+     * @author Rosalia Padoin
+     * @return Retorna uma lista contendo entidades do tipo Manager.
+     * @throws NotFoundException - NotFoundException
+     */
+    List<Manager> getAll();
+
+    /**
+     * Método responsável por buscar um representante conforme Id informado.
+     * @author Rosalia Padoin
+     * @return Retorna uma entidade do tipo Manager.
+     * @throws NotFoundException - NotFoundException
+     */
+    Manager getManagerById(Long id);
+
+    /**
+     * Método responsável por excluir um representante conforme Id informado.
+     * @author Rosalia Padoin
+     * @throws NotFoundException - NotFoundException
+     */
+    void deleteManager(Long id);
 }
