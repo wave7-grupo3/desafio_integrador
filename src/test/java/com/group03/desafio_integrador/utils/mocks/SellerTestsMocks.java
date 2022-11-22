8a2cdd73d350b332c6fac717f879845a481cd566
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-11-22 11:21:09 -0300

    :rocket: create mocks to using in test for Seller

---
+++ src/test/java/com/group03/desafio_integrador/utils/mocks/SellerTestsMocks.java
@@ -0,0 +1,110 @@
+package com.group03.desafio_integrador.utils.mocks;
+
+import com.group03.desafio_integrador.entities.Batch;
+import com.group03.desafio_integrador.entities.ProductAdvertising;
+import com.group03.desafio_integrador.entities.Seller;
+import com.group03.desafio_integrador.entities.entities_enum.CategoryEnum;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SellerTestsMocks {
+    static ProductAdvertising productId = ProductAdvertising.builder().productId(1L).build();
+
+    private static final List<ProductAdvertising> productList = new ArrayList<>();
+
+    private static final List<Seller> sellerList = new ArrayList<>();
+
+    public static List<ProductAdvertising> mockProductList() {
+
+        ProductAdvertising productFresh = new ProductAdvertising(16L,
+                LocalDate.parse("2022-11-30"),
+                LocalDateTime.of(2022, 11, 9, 11, 43, 0),
+                "Maçã",
+                "teste",
+                Seller.builder().sellerId(1L).build(),
+                CategoryEnum.FS,
+                BigDecimal.valueOf(2),
+                5
+        );
+
+        ProductAdvertising productRefrigerated = new ProductAdvertising(17L,
+                LocalDate.parse("2022-11-30"),
+                LocalDateTime.of(2022, 11, 9, 11, 43, 0),
+                "Iogurte",
+                "teste",
+                Seller.builder().sellerId(1L).build(),
+                CategoryEnum.RF,
+                BigDecimal.valueOf(2),
+                10
+        );
+
+        ProductAdvertising productFrozen = new ProductAdvertising(18L,
+                LocalDate.parse("2022-11-30"),
+                LocalDateTime.of(2022, 11, 9, 11, 43, 0),
+                "Polpa de Morango",
+                "teste",
+                Seller.builder().sellerId(1L).build(),
+                CategoryEnum.FF,
+                BigDecimal.valueOf(1),
+                10
+        );
+
+        productList.add(productFresh);
+        productList.add(productRefrigerated);
+        productList.add(productFrozen);
+
+        return productList;
+
+    }
+
+    public static Seller mockcreateSeller() {
+
+        return new Seller(
+                null,
+                "Seller 1",
+                productList,
+                0);
+    }
+
+    public static Seller mockSeller() {
+
+        return new Seller(
+                1L,
+                "Seller 1",
+                mockProductList(),
+                0);
+    }
+
+    public static List<Seller> mockSellerList() {
+
+        Seller seller2 = new Seller(
+                2L,
+                "Seller 2",
+                mockProductList(),
+                0);
+
+        sellerList.add(mockSeller());
+        sellerList.add(seller2);
+
+        return sellerList;
+    }
+
+    public static Batch mockBatch() {
+
+        return new Batch(1L,
+                productId,
+                10.0F,
+                15,
+                LocalDate.parse("2022-11-30"),
+                LocalDateTime.of(2022, 11, 9, 11, 43, 0),
+                30.0F,
+                LocalDate.parse("2022-12-30"),
+                BigDecimal.valueOf(150.00));
+    }
+}
+
